74950a8f3bd7b4ac84a2a34867e0a62d4aca7be5
pietricica <pietricica@63ccebb7-0b34-0410-8288-c464d40f8c6f>
2007-08-26 09:34:44 +0000

    fixed the changelog

--- src/dshub/ADC.java
+++ src/dshub/ADC.java
@@ -59,77 +59,10 @@ abstract public class ADC
            "Or the SourceForge page located at http://www.sf.net/projects/dshub\n"+
            "Have a nice hubbing !"
            
-          /* "           * fixed cfg spaces in setting topic hub name etc.\n" +
-            "           * fixed small gui stuff in opchat desc & name;\n"+
-            "           * //met MAGY he helped me with a GUI for dshub, just testing some GUI now;\n"+
-            "           * tested GUI on windows and linux with no X server, in last case, message GUI not viewable shows up;\n"+
-            "           * still need to test on X server;\n"+
-            "           * added a gui command that brings up GUI;\n"+
-            "           * gui still has minimal functionality, lot of work still to do;\n" +
-            "           * fixed bug with running via dbl click with class path;\n"+
-            "           * fixed bug not showing correct port after change;\n"+
-            "           * a lot of new changes in GUI, added some tabs, config settings, and account listing;\n"+
-            "           * fixed minor bugs with config variables;\n"+
-            "           * hope the new GUI is nice, waiting for feedback :D\n"+
-            "           * modified GUi added log tab with log;\n"+
-            "           * created popmsg fcuntion that pops a message console & log;\n"+
-            "           * moved settings in 4 tabs according to type;\n"+
-            "           * fixed minor problems in other files;\n"+
-            "           * still TODO : sta, keepalive;\n"+
-            "           * completeley redesigned settings tab;\n"+
-            "           * now the string variables like nick chars, may contain space, newline and slash;\n"+
-            "           * fixed the variables check from gui, modifying into cmd parser too;\n"+
-            "           * changed with default hub name in form title;\n"+
-            "           * corrected rename, topic bugs from rc4, sorry about them;\n"+
-            "           * changed the keep alive completely, added some other checker, will remove var keep_alive too probably;\n"+
-            "           * started code optimizing: 1) optimized thread priorities, some useless params on threads;\n"+
-            "           * corrected message reading from client deleted crappy code that possibly dropped laggy and multiple messaging;\n"+
-            "           * added another variable save_logs and to main settings, that checks if logs are saved in format starttime&date.log;\n"+
-            "           * refixed minor bugs that some previous changes made, im SORRY;\n"+
-            "           * added some commands to log registry;\n"+
-            "           * fixed client assassin again, sorry for it;\n"+
-            "           * fixed creating ugly log file with wrong path & name with startup message;\n"+
-            "           * modified share to MiB, added extra message to disconnect specifying min share;\n"+
-            "           * TODO : messages for all disconnects( better ones);\n"+
-                               " * DSHub has now powerful searching features..\n"+
-                                "First, we need to make a distinction between the.\n"+
-                                "automagic and the user searches.\n"+
-                                "  First type is made by client at a regular interval\n"+
-                                " and DSHub keeps a liniar spam setting.\n" +
-                                "  Second type are user searches ( manual searches )\n"+
-                                " that the user takes.\n"+
-                                "For this type (because of the human factor)\n"+
-                                " DSHub keeps a logarithmic spam setting.\n"+
-                                "This way, the 2nd search is at search_log_base\n"+
-                                "interval, but third, is at search_log_base^2\n"
-                               +"and so on, until the power gets to max_steps.\n"+
-                                " After this point, the user needs to wait\n"+
-            "search_spam_reset seconds to get his burst back.\n"+
-            "The searches are being kept in queue ( not ignored !)\n"+
-            "and are processed once the timeout is completed\n"+
-            "so user doesnt need to search again\n"+
-            "but just wait for his search to be completed.\n"+
-            "The messages appears as a fictive result\n"+
-            "in his search box, which will be filled\n"+
-            "once the search string is being sent to others.\n"+
-            "           * Lots of new stuff, started working on the spam settings on searches;\n"+
-            "           * above is the main info for the variables i added;\n"+
-            "           * automagic_search is the automagic interval;\n"+
-            "           * search_log_base the log base for search spam algorithm;\n"+
-            "           * search_steps the max steps allowed in log scale;\n"+
-            "           * search_spam_reset the time needed to reset burst ( also needed if search_steps is touched);\n"+
-            "           * msg_search_spam the message that appears as a fake result in searches until the SCH is being sent;\n"+
-            "           * added a hub_security bot that receives cmds in private ( but are shown as IMSGs still );\n"+
-            "           * also added 2 vars: bot_name and bot_desc to set the NI and DE of the bot;\n"+
-            "           * added extra fields to clienthandler so it can properly remember chat settings;\n"+
-            "           * fixed some banning bugs...\n"+
-            "           * added posibility to delete accounts from GUI via delete key...\n"+
-            "           * fixed bugs when renaming and not check the bot cid & nick\n"+
-            "           * added extended possibilites to mass comand, renamed from massall to mass\n"+
-            "           * hub security now sends all messages except login;\n"+
-            "           * finally found bug that killed me over last month, when passive user couldnt connect irregulary (found out why);\n"+
-            "           * completely changed string parsing, now everything is going thru 2 functions;\n"+
-            "Zeta Ultra Version"*/
+          /*           * fixed cfg spaces in setting topic hub name etc.
+                       * fixed small gui stuff in opchat desc & name;
+           **/
+            
             ;
     
     
