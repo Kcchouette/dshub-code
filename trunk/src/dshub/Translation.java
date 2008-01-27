eb334f67bb0d42bf810c3173db074fb133f8f965
pietricica <pietricica@63ccebb7-0b34-0410-8288-c464d40f8c6f>
2008-01-27 15:49:40 +0000

    more work on translations

---
+++ trunk/src/dshub/Translation.java
@@ -0,0 +1,91 @@
+/*
+ * Translation.java
+ *
+ * Created on 27 ianuarie 2008, 17:24
+ *
+ * DSHub ADC HubSoft
+ * Copyright (C) 2007  Pietricica
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package dshub;
+
+import java.text.MessageFormat;
+import java.util.*;
+
+/**
+ *
+ * @author Pietricica
+ */
+public class Translation 
+{
+static String cc;
+static String lc;
+
+public static ResourceBundle Strings=null;
+static Locale curLocale;
+
+static
+{
+        curLocale=Locale.getDefault()    ;
+         
+         try
+         {
+              Strings = ResourceBundle.getBundle("Translation",
+                                           curLocale);
+         }
+         catch (java.util.MissingResourceException mre)
+         {
+             System.out.println("Fatal Error : Unable to locate Translation.properties file or any other translation. FAIL.");
+             System.exit(1);
+         }
+         
+        
+         System.out.println("Current found country : " + (cc=curLocale.getCountry()));
+         lc=curLocale.getLanguage();
+         System.out.println(getString("init"));
+}
+
+public static String getString(String what)
+{
+    try
+    {
+    if(what.equals("startup"))
+    {
+        Object[] Args = {
+    Vars.HubVersion
+       };
+        MessageFormat formatter = new MessageFormat("");
+        formatter.setLocale(curLocale);
+        
+        formatter.applyPattern(Strings.getString("startup"));
+        return formatter.format(Args);
+        
+        
+    }
+    if(what.equals("init"))
+    {
+        return Strings.getString("init");
+    }
+    }
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+    
+    return null;
+}
+}
