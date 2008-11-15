3e61582e5af6337058aa99a3b631bc06bfdbbe13
swe_toast <swe_toast@63ccebb7-0b34-0410-8288-c464d40f8c6f>
2008-11-15 14:30:20 +0000

    Another update on basic_info.py

--- trunk/scripts/py/basic_info.py
+++ trunk/scripts/py/basic_info.py
@@ -5,11 +5,20 @@
 #   Simple script (example for DSHub)
 import sys, socket, os, sys, platform
 print "\n"
-print "Basic Info:"
-print "\n"
+print "Basic Info:","\n"
 hostname = socket.gethostname()
 print "Hostname:", hostname
 print 'Operating System:', platform.system(), platform.release()
+pathname = os.path.dirname(sys.argv[0])
+print 'Working Directory:', sys.argv[0] ,os.path.abspath(pathname)
 print 'Parent Process ID:', os.getpid()
 print 'Real User ID:', os.getuid()
 print "\n"
+print "Saving to information to basic_info.log"
+f = open('basic_info.log', 'w')
+print >> f, "Basic Info:","\n"
+print >> f, "Hostname:", hostname
+print >> f, 'Operating System:', platform.system(), platform.release()
+print >> f, 'Parent Process ID:', os.getpid()
+print >> f, 'Real User ID:', os.getuid()
+f.close()
