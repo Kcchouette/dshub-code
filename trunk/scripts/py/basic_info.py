e88f0de58e0d6b6689b3fd1773df6958a11a1755
swe_toast <swe_toast@63ccebb7-0b34-0410-8288-c464d40f8c6f>
2008-11-15 13:55:36 +0000

    Update on the basic_info.py some minor ajustment and more info

--- trunk/scripts/py/basic_info.py
+++ trunk/scripts/py/basic_info.py
@@ -1,13 +1,15 @@
-#   Basic  Information 0.1
-#   By Toast 08-10-02
+#   Basic  Information 0.1a
+#   By Toast 08-11-15
 #
 #   Prints out Hostname and PID for process
 #   Simple script (example for DSHub)
-import sys, socket, os, sys
+import sys, socket, os, sys, platform
 print "\n"
 print "Basic Info:"
 print "\n"
 hostname = socket.gethostname()
 print "Hostname:", hostname
-print 'DSHub PID:', os.getpid()
+print 'Operating System:', platform.system(), platform.release()
+print 'Parent Process ID:', os.getpid()
+print 'Real User ID:', os.getuid()
 print "\n"
